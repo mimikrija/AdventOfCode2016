69037896fd7c0f61e5b193aba1d729b6b1e965bb
Maja Gacesa <maja.gacesa@gmail.com>
2020-11-23 20:19:24 +0100

    [day01] part 2 horribly done

--- 01.py
+++ 01.py
@@ -8,6 +8,36 @@ def get_direction(absolute_index):
         absolute_index = - (-absolute_index %4)
     return sides[absolute_index]
 
+def add_coordinates(start,end,locations):
+    is_horizontal = False
+    is_vertical = False
+    if start[1] == end[1]: #horizontal movement
+        is_horizontal = True
+        range_start = start[0]
+        range_end = end[0]
+    else:
+        is_vertical = True
+        range_start = start[1]
+        range_end = end[1]
+
+    if range_end > range_start:
+        step = 1
+    else:
+        step = -1
+
+    coordinate = start
+    for n in range (range_start + step,range_end,step):
+            #print(n)
+            if is_horizontal:
+                coordinate = (n, coordinate[1]) 
+            else:
+                coordinate = (coordinate[0], n)
+            if coordinate in locations:
+                solution_2 = abs(coordinate[0]) + abs(coordinate[1])
+                return solution_2
+            locations.append(coordinate)
+    return None
+
 
 sides = ['n','e','s','w']
 
@@ -16,7 +46,7 @@ horizontal_distance = 0
 vertical_distance = 0
 absolute_index = 0
 locations = [(0,0)]
-
+solution_2 = None
 
 for direction in directions:
     x = 0
@@ -27,18 +57,23 @@ for direction in directions:
     else:
         absolute_index -= 1
     if get_direction(absolute_index) == 'n':
-        x = int(direction[1:])
-    if get_direction(absolute_index) == 's':
-        x = - int(direction[1:])
-    if get_direction(absolute_index) == 'e':
         y = int(direction[1:])
+    if get_direction(absolute_index) == 's':
+        y = - int(direction[1:])
+    if get_direction(absolute_index) == 'e':
+        x = int(direction[1:])
     if get_direction(absolute_index) == 'w':
-        y = -int(direction[1:])
+        x = -int(direction[1:])
     vertical_distance += y
     horizontal_distance += x
-    last = locations[-1]
-    current = (last[0]+x,last[1]+y)
+    current = (horizontal_distance,vertical_distance)
+    if solution_2 == None:
+        solution_2 = add_coordinates(locations[-1],current,locations)
     locations.append(current)
 
-
+#print(locations)
 print ("Part one solution: I am ", abs(vertical_distance) +abs(horizontal_distance), " blocks away!")
+# Part one solution: I am  288  blocks away!
+
+print ("Part two solution: Actually, the headquarters is ", solution_2, " blocks away!")
+# Part two solution: Actually, the headquarters is  111  blocks away!
