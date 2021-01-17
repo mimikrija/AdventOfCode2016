63bc13147ea3e876ffeb357c1e19b0aa75a59394
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-17 17:38:54 +0100

    [day03] part 2 messy first go

--- python/03.py
+++ python/03.py
@@ -4,8 +4,21 @@ import re
 
 is_triangle = lambda x: x[0] + x[1] > x[2]
 
-potential_triangles = [sorted(map(int, re.findall(r'\d+', line))) for line in helpers.get_input('inputs/03', '\n')]
 
-part_1 = sum(is_triangle(candidate) for candidate in potential_triangles)
+potential_triangles = [list(map(int, re.findall(r'\d+', line))) for line in helpers.get_input('inputs/03', '\n')]
 
-helpers.print_solutions(part_1)
+part_1 = sum(is_triangle(sorted(candidate)) for candidate in potential_triangles)
+
+
+
+potential_vertical = []
+for line_num in range(0, len(potential_triangles), 3):
+    for n in range (0,3):
+        potential_vertical.append([potential_triangles[line_num + offset][n] for offset in range(0,3)])
+
+
+part_2 = sum(is_triangle(sorted(candidate)) for candidate in potential_vertical)
+
+helpers.print_solutions(part_1, part_2)
+# Part 1 solution is: 917
+# Part 2 solution is: 1649
