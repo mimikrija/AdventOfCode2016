0bb06cc07f04f48f29d5e552d054b4700502bef6
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-17 17:23:40 +0100

    [day03] part 1 first go

--- python/03.py
+++ python/03.py
@@ -2,4 +2,10 @@
 import santas_little_helpers as helpers
 import re
 
+is_triangle = lambda x: x[0] + x[1] > x[2]
+
 potential_triangles = [sorted(map(int, re.findall(r'\d+', line))) for line in helpers.get_input('inputs/03', '\n')]
+
+part_1 = sum(is_triangle(candidate) for candidate in potential_triangles)
+
+helpers.print_solutions(part_1)
