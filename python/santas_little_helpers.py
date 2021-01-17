69c081012f01eb2e6a009a576b412a9c778b9b32
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-17 17:11:29 +0100

    add helpers function for printing solution(s)

--- python/santas_little_helpers.py
+++ python/santas_little_helpers.py
@@ -3,3 +3,8 @@ def get_input(file_name, separator):
     with open(file_name) as input_file:
         input_data = input_file.read().split(separator)
     return input_data
+
+def print_solutions(part_1, part_2 = None):
+    print(f'Part 1 solution is: {part_1}')
+    if part_2:
+        print(f'Part 2 solution is: {part_2}')
