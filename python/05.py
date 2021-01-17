81e58b48cac98bea3281d9d6b1c39eb3061c56ca
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-17 20:23:56 +0100

    [day05] part 2 first go

--- python/05.py
+++ python/05.py
@@ -12,14 +12,29 @@ def lowest_positive(start_num, in_code, leading_zeroes):
         candidate = in_code + str(number)
         hash_code = hashlib.md5(candidate.encode()).hexdigest()
         found = has_n_leading_zeroes(hash_code, leading_zeroes)
-    return number, hash_code[5]
+    return number, hash_code[5], hash_code[6]
 
 
 my_input = "uqwqemis"
 start = 0
 part_1 = ''
+part_2 = [None for _ in range(8)]
+
+counter = 0
+
+while any(char == None for char in part_2):
+    start, pass_pos, pass_char = lowest_positive(start, my_input, 5)
+    try:
+        if int(pass_pos) < 8 and part_2[int(pass_pos)] == None:
+            part_2[int(pass_pos)] = pass_char
+    except:
+        continue
+
+print(''.join(c for c in part_2)) # 694190cd
+
+quit()
 for _ in range(8):
-    start, pass_char = lowest_positive(start, my_input, 5)
+    start, pass_char, _ = lowest_positive(start, my_input, 5)
     part_1 += str(pass_char)
 
 helpers.print_solutions(part_1)
