612fb671b5d341aac1e03f0d966008185d7bcf90
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-17 18:54:08 +0100

    [day04] part 2 first go, this need fixing

--- python/04.py
+++ python/04.py
@@ -18,11 +18,38 @@ def is_real_room(room):
     if top_five(encrypted_name) == checksum:
         return sector_ID
     else:
-        return 0
+        return False
 
 
 rooms = [re.findall(r'\w+', line) for line in helpers.get_input('inputs/04', '\n')]
 
 part_1 = sum(is_real_room(room) for room in rooms)
 
-helpers.print_solutions(part_1)
+
+
+def rotate_letter(c, number):
+    offset = number % 26
+    total = ord(c) + offset
+    if total <= ord('z'):
+        return chr(total)
+    else:
+        return chr(ord('a')+total-ord('z')-1)
+
+def decypher_name(room_data):
+    result = []
+    words, number = room_data[:-1], int(room_data[-1])
+    for word in words:
+        new = ''.join( rotate_letter(c, number) for c in word)
+        result.append(new)
+    return result
+
+
+for room in rooms:
+    if is_real_room(room):
+        new = decypher_name(room[:-1])
+        if 'object' in new:
+            part_2 = int(room[-2])
+
+helpers.print_solutions(part_1, part_2)
+# Part 1 solution is: 409147
+# Part 2 solution is: 991
