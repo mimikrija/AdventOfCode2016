7ce306848b215c4e193482f8c3a7ff599501e4a7
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-18 08:58:27 +0100

    [day06] party 2 + refactor

--- python/06.py
+++ python/06.py
@@ -3,15 +3,19 @@
 import santas_little_helpers as helpers
 from collections import Counter
 
+
+def get_message(in_columns, is_part_2 = False):
+    most_or_least_common = lambda x: Counter(x).most_common(1)[0][0] if not is_part_2 else Counter(x).most_common()[len(set(x))-1][0]
+    return "".join(c for c in (most_or_least_common(column) for column in in_columns))
+
+
 messages = helpers.get_input('inputs/06', '\n')
 
 message_length = len(messages[0])
+message_columns = [[line[column] for line in messages] for column in range(0, message_length)]
 
-columns = [[line[column] for line in messages] for column in range(0, message_length)]
 
-
-correct_message = [Counter(column).most_common(1)[0][0] for column in columns]
-
-part_1 = "".join(c for c in correct_message)
-
-helpers.print_solutions(part_1)
+part_1, part_2 = (get_message(message_columns, is_part_2) for is_part_2 in {False, True})
+helpers.print_solutions(part_1, part_2)
+# Part 1 solution is: tsreykjj
+# Part 2 solution is: hnfbujie
