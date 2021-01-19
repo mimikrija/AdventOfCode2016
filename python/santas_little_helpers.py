645c69410df2961937ec57ff76da02106db481ea
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-19 13:44:12 +0100

    add an addition with wrap up helper
    I figured this will be useful in other puzzles as well

--- python/santas_little_helpers.py
+++ python/santas_little_helpers.py
@@ -8,3 +8,7 @@ def print_solutions(part_1, part_2 = None):
     print(f'Part 1 solution is: {part_1}')
     if part_2:
         print(f'Part 2 solution is: {part_2}')
+
+def add_wrap(num_1, num_2, limit, start=0):
+    " Returns sum of `num_1` and `num_2` wrapped around to `start` they go over `limit`"
+    return start + (num_1 + num_2)%(limit+1)
