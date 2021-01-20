6ef97eb76f79d3f96ac2a42ee65ece91a83fa204
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-20 20:45:42 +0100

    [day09] part 2 is a "double recursion" of part 1
    instead of simply returning multiply*count, we need to check whether
    any of the characters within the count range contain any markers. so,
    we call the function twice - first to evaluate what's left in this small
    substring to be considered, and then, as before, to evaluate the rest
    of so far uncompressed string.

--- python/09.py
+++ python/09.py
@@ -5,19 +5,22 @@ import re
 compressed_string = helpers.get_input('inputs/09','\n')[0]
 
 
-def decompress(in_string):
+def decompress(in_string, is_part_2=False):
     first_marker = re.search(r'(\d+)x(\d+)', in_string)
     if first_marker:
         count, multiply = map(int, first_marker.groups())
         before_marker_pos, after_marker_pos = first_marker.start()-1, first_marker.end()+1
-        return before_marker_pos + multiply*count + decompress(in_string[after_marker_pos+count:])
+        if not is_part_2:
+            return before_marker_pos + multiply*count + decompress(in_string[after_marker_pos+count:])
+        else:
+            # if part we need to evaluate count recursively, but only within the marker + count!
+            return before_marker_pos + multiply*decompress(in_string[after_marker_pos:after_marker_pos+count], True) + decompress(in_string[after_marker_pos+count:], True)
     else:
         return len(in_string)
 
 
+part_1, part_2 = (decompress(compressed_string, is_part_2) for is_part_2 in {False, True})
 
-# part_1 = len(decompressed_string)
-part_1 = decompress(compressed_string)
-
-helpers.print_solutions(part_1)
+helpers.print_solutions(part_1, part_2)
 # Part 1 solution is: 99145
+# Part 2 solution is: 10943094568
