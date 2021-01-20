7db06949aaa84dff0ed0446329e7f9890c689f4d
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-20 20:34:32 +0100

    [day09] rewrite loop as recursion

--- python/09.py
+++ python/09.py
@@ -5,25 +5,19 @@ import re
 compressed_string = helpers.get_input('inputs/09','\n')[0]
 
 
-# decompressed_string = ''
-rest = str(compressed_string)
-decompressed_length = 0
-
-while len(rest) > 0:
-    try:
-        first_marker = re.search(r'(\d+)x(\d+)', rest)
+def decompress(in_string):
+    first_marker = re.search(r'(\d+)x(\d+)', in_string)
+    if first_marker:
         count, multiply = map(int, first_marker.groups())
         before_marker_pos, after_marker_pos = first_marker.start()-1, first_marker.end()+1
-        # decompressed_string += rest[:before_marker_pos] + multiply * rest[after_marker_pos:after_marker_pos+count]
-        decompressed_length += before_marker_pos + multiply*count
-        rest = rest[after_marker_pos+count:]
-    except:
-        decompressed_length += len(rest)
-        # decompressed_string += rest
-        rest = ''
+        return before_marker_pos + multiply*count + decompress(in_string[after_marker_pos+count:])
+    else:
+        return len(in_string)
+
+
 
 # part_1 = len(decompressed_string)
-part_1 = decompressed_length
+part_1 = decompress(compressed_string)
 
 helpers.print_solutions(part_1)
 # Part 1 solution is: 99145
