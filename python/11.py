f7287842c610245f02b0dbeacc46832beaeea89e
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-22 20:16:02 +0100

    [day11] define rules for allowed items on floor
    I think this is the key function when testing whether a certain
    arrangement is even possible. The idea will be to generate all possible
    combos to send with the elevator from one floor to another and this
    function will make sure that:
    a) whatever was taken from the origin floor doesn't fry any microchips
    b) whatever gets on the destination floor doesn't fry any microchips

---
+++ python/11.py
@@ -0,0 +1,28 @@
+import itertools
+
+GENERATORS = {'HG', 'LG'}
+MICROCHIPS = {'HM', 'LM'}
+
+
+def all_microchips_matched(generators, microchips):
+    return all(any(generator[:-1] == microchip[:-1] for generator in generators) for microchip in microchips)
+
+def items_allowed_on_this_floor(items, floor):
+    """ checks whether all `items` in the elevator are compatible with thatever items are in `floor` """
+    everything = items | floor
+    generators = everything & GENERATORS
+    microchips = everything & MICROCHIPS
+    # there are no generators, it is ok
+    if not generators:
+        return True
+    # there are no microchips so nothing to fry, it is ok
+    if not microchips:
+        return True
+    # there are both groups present, we need to check if any of the microchips something will be fried
+    # if there are more microchips than generators, some microchips will be fried because there is no way
+    # a matching generator exists
+    if len(microchips) > len(generators):
+        return False
+    return all_microchips_matched(generators, microchips)
+
+
