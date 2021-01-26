6b70399988fc9aa179214b99a9411fc804917825
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-26 20:56:46 +0100

    [day13] define is open function
    based on the puzzle instructions formula this is how we determine if a
    given coordinate is a wall or an open space. in this implementation,
    True means it is an open space, and False means it is a wall.

--- python/13.py
+++ python/13.py
@@ -4,6 +4,12 @@ my_input = 1358
 my_input = 10 # example
 
 
+def is_open(in_coordinate):
+    x, y = in_coordinate
+    ones = bin(x*x + 3*x + 2*x*y + y + y*y + my_input).count('1')
+    return ones % 2 == 0
+
+
 # What is the fewest number of steps required for you to reach 31,39? (starting from 1,1)
 goal = (31, 39)
 goal = (7, 4) # example
