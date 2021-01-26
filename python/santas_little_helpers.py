3ebd783adf5de69e45d38f649299aabd51f54461
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-26 19:50:29 +0100

    use \n as the default separator for inputs

--- python/santas_little_helpers.py
+++ python/santas_little_helpers.py
@@ -1,4 +1,4 @@
-def get_input(file_name, separator):
+def get_input(file_name, separator='\n'):
     """ Reads in data present in `file_name` separated by `separator` string. """
     with open(file_name) as input_file:
         input_data = input_file.read().split(separator)
