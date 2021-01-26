337b1f54903aca9911edfb9ad4c8b597a883f8d7
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-26 20:46:27 +0100

    [day13] problem statement and input

---
+++ python/13.py
@@ -0,0 +1,9 @@
+# Day 13: A Maze of Twisty Little Cubicles
+
+my_input = 1358
+my_input = 10 # example
+
+
+# What is the fewest number of steps required for you to reach 31,39? (starting from 1,1)
+goal = (31, 39)
+goal = (7, 4) # example
