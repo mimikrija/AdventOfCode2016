eb30226f596e40712e7f8399f8f3f97d50a1bde9
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-29 21:06:43 +0100

    [day15] both parts, solution based on 2020d13
    need to clean up a bit more

---
+++ python/15.py
@@ -0,0 +1,91 @@
+from santas_little_helpers import *
+import re
+from math import lcm
+
+# get all numbers from input, second number is irrelevant because they are all zero
+# this is what the input looks like: Disc #1 has 5 positions; at time=0, it is at position 2.
+
+disks = (map(int, re.findall(r'\d+', line)) for line in get_input('inputs/15'))
+
+
+# in the problem it takes 1 second to get to disc 1, another to get to disc 2, and so on...
+# Disc #1 has 5 positions; at time=0, it is at position 4.
+# Disc #2 has 2 positions; at time=0, it is at position 1. 
+# time:    0 1 2 3 4 5 6 7 ...
+# Disc 1:  4 0 1 2 3 4 0 1 2 3 4 ....
+# Disc 2:  1 0 1 0 1 0 1 0 1 0 1 0 ...
+
+# so the solution is 5 because after 1 second disk 1 will be at position 0,
+# and after one more second, disk 2 will be at position 0
+
+# we can imagine that there is no passage of time when the ball drops - to do that,
+# we need to change the initial shift of the disk so we have this:
+# time:    0 1 2 3 4 5 6 7 ...
+# Disc 1:  0 1 2 3 4 0 1 2 3 4 0 1 2 ...
+# Disc 2:  1 0 1 0 1 0 1 0 1 0 1 0 1 ...
+# if we do that, we are just looking for the column in which all disks align to zero
+
+# disk_period is the number of holes in the disk
+# disk number corresponds to the delay in seconds
+# start_pos is the position at time 0 from input
+
+# disks_and_offsets combines the disk period (the number of holes in the disk)
+# with the position a disk would have at time zero if the ball would >>instantly<<
+# pass through all the disks - we get that by adding disk_no, ie. the delay
+
+# in the above example: 
+# disc 1 offset is 4 + 1 = 5 which wraps around to 0
+# disc 2 offset is 1 + 2 = 3 which wraps around to 1
+
+
+disks_and_offsets = {disk_period: add_wrap(start_pos, disk_no, disk_period-1)
+                                for disk_no, disk_period, _, start_pos in disks}
+
+
+def first_overlap(time_start, period_1, offset_1, period_2, offset_2, time_step):
+    # if time_start == 0:
+    #     time_step = period_1
+    # else:
+    #     time_step = lcm(period_1, period_2)
+    for time in range (time_start, period_1*period_2*time_step + 1, time_step):
+        if (time + offset_1) % period_1 == 0 and (time + offset_2) % period_2 == 0:
+            return time
+
+
+
+def find_overlap_all(in_periods_and_offsets):
+    # the most important thing with this approach is to have periods and offsets sorted
+    # by the offsets
+    # periods_and_offsets = sorted(in_periods_and_offsets.items(), key=lambda x: x[1])
+    # print(periods_and_offsets)
+    # time = 0
+    # period = periods_and_offsets[0][0]
+    # for first, second in zip(periods_and_offsets, periods_and_offsets[1:]):
+    #     print(first, second)
+    #     time = first_overlap(time, *first, *second, period)
+    #     period *= second[1] # this should be LCM, but since they're all prime it's the same thing
+
+    # return time
+    bus_IDs, time_diffs = zip(*sorted(in_periods_and_offsets.items(), key=lambda x: x[1]))
+    time = 0
+    period = bus_IDs[0]
+    for n in range(len(bus_IDs)-1):
+        b_1 = bus_IDs[n]
+        b_2 = bus_IDs[n+1]
+        d_1 = time_diffs[n]
+        d_2 = time_diffs[n+1]
+        time = first_overlap(time, b_1, d_1, b_2,d_2, period)
+        period = lcm(period,b_2) # this should be LCM, but since they're all prime it's the same thing
+    return time
+
+part_1 = find_overlap_all(disks_and_offsets)
+
+
+# part 2, add disk no. 7
+disk_no, disk_period, _, start_pos = map(int, re.findall(r'\d+', 'Disc #7 has 11 positions; at time=0, it is at position 0.'))
+disks_and_offsets[disk_period] = add_wrap(start_pos, disk_no, disk_period-1)
+
+part_2 = find_overlap_all(disks_and_offsets)
+print_solutions(part_1, part_2)
+# Part 1 solution is: 148737
+# Part 2 solution is: 2353212
