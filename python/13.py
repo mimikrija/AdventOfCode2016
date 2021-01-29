39d54e3049faf438d8c4bcb2d4139e517d704170
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-29 12:30:17 +0100

    [day13] describe functions

--- python/13.py
+++ python/13.py
@@ -9,22 +9,25 @@ my_input = 1358
 
 
 def is_open(in_coordinate):
+    "returns `True` if `in_coordinate` is open, and `False` if it is a wall"
     x, y = in_coordinate
     ones = bin(x*x + 3*x + 2*x*y + y + y*y + my_input).count('1')
     return ones % 2 == 0
 
 
 def four_neighbors(in_coordinate):
+    "generates up, down, left, right neighbors of `in_coordinate`"
     x, y = in_coordinate
     return {(x, y + delta) for delta in {1,-1}} | {(x + delta, y) for delta in {1,-1}}
 
 
-def neighbors(in_coordinate):
+def open_neighbors(in_coordinate):
+    "returns open (ie. not walls) neighbors of `in_coordinate`, taking into account that the map expands in the positive quadrant only (no negative coords allowed)."
     return {coordinate for coordinate in four_neighbors(in_coordinate) if all(coord >= 0 for coord in coordinate) and is_open(coordinate)}
 
 
 def BFS_shortest_distance(start, end):
-    """ Red blob games BFS with shortest distance implementation """
+    "Red blob games BFS with shortest distance from `start` to `end` implementation"
     frontier = deque()
     frontier.append(start)
     came_from = dict()
@@ -32,7 +35,7 @@ def BFS_shortest_distance(start, end):
 
     while frontier:
         current_position = frontier.pop()
-        for next_position in neighbors(current_position):
+        for next_position in open_neighbors(current_position):
             if next_position not in came_from:
                 frontier.appendleft(next_position)
                 came_from[next_position] = current_position
@@ -42,6 +45,7 @@ def BFS_shortest_distance(start, end):
 
 
 def path_length(start, end):
+    " backtracks from `end` to `start` and returns path length"
     came_from = BFS_shortest_distance(start, end)
     current = end
     path = []
@@ -52,6 +56,7 @@ def path_length(start, end):
 
 
 def BFS_visit_everything(start, steps):
+    "variation of BFS to visit everything expanding from `start` in `steps` frontiers (rings). returns total count of locations visited."
     frontier = deque()
     # we add a set of rings that make up a frontier. each frontier is one ring/step
     frontier.append({start})
@@ -62,7 +67,7 @@ def BFS_visit_everything(start, steps):
     while frontier:
         current_frontier = frontier.pop()
         count += 1
-        next_frontier = {next_position for current_position in current_frontier for next_position in neighbors(current_position) if next_position not in reached}
+        next_frontier = {next_position for current_position in current_frontier for next_position in open_neighbors(current_position) if next_position not in reached}
         frontier.append(next_frontier)
         reached |= next_frontier
         if count == steps:
