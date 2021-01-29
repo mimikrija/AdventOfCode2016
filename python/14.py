dbb0ede397e7f3ee95671ecf1d955a4c2bc0ebcf
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-29 09:54:11 +0100

    [day14] use dicts to keep generated hashes
    this speeds things up. instead of generating new hashes every time, we
    first generate a list of hashes, then we filter on those which satisfy
    the 'has three consec. letters' condition, and then we additionally
    filter on those which satisfy 'has five consec. letters'. the latter is
    a subset of the former, because if a hash contains five consec. letters,
    it definitely contains three - no need to search through the original
    list of hashes.
    
    the implementation then loops through the 'threes' dictionary and checks
    whether there exists a hash which contains five consec. characters, such
    that the repeated character is the same as the one found in the threes.
    this is why I keep track of the character itself as well.
    
    part two is super slow but at least it runs, compared to my original
    attempt.

--- python/14.py
+++ python/14.py
@@ -3,53 +3,57 @@
 from santas_little_helpers import *
 import hashlib
 
-# However, not all of these MD5 hashes are keys, and you need 64 new keys for your one-time pad. A hash is a key only if:
 
-# It contains three of the same character in a row, like 777. Only consider the first such triplet in a hash.
-# One of the next 1000 hashes in the stream contains that same character five times in a row, like 77777.
-
-# I think we will need to use some sort of memoization
-
-def has_three_consec_characters(in_string, _):
+def has_three_consec_characters(in_string):
     for one, two, three in zip(in_string, in_string[1:], in_string[2:]):
         if one == two == three:
-            return one
-    return False
+            return in_string, one
+    return False, False
 
-def has_five_consec_characters(in_string, in_char):
-    if not in_char:
-        return False
-    return any(in_char == one == two == three == four == five for one, two, three, four, five in zip(in_string, in_string[1:], in_string[2:], in_string[3:], in_string[4:]))
+
+def has_five_consec_characters(in_string):
+    return any(one == two == three == four == five for one, two, three, four, five in zip(in_string, in_string[1:], in_string[2:], in_string[3:], in_string[4:]))
+
+
+def generate_hash(index, in_code, is_part_2=False):
+    seed = in_code + str(index)
+    hash_code = hashlib.md5(seed.encode()).hexdigest()
+    if not is_part_2:
+        return hash_code
+    else:
+        for _ in range(2016):
+            hash_code = hashlib.md5(hash_code.encode()).hexdigest()
+        return hash_code
+
+
+def generate_hash_dicts(max_num, in_code, is_part_2=False):
+    hashes = (generate_hash(index, in_code, is_part_2) for index in range(max_num))
+    threes = {n: (hash_code, character) for n, (hash_code, character) in enumerate(has_three_consec_characters(hash_code) for hash_code in hashes) if character}
+    fives = {n: hash_code for n, (hash_code, character) in threes.items() if has_five_consec_characters(hash_code)}
+    return threes, fives
+
+
+def has_fives_in_next_1000(index, _, character, fives):
+    return any(5*character in fives.get(check,'') for check in range(index+1, index+1002))
+
+
+def find_first_64(max_num, in_code, is_part_2=False):
+    threes, fives = generate_hash_dicts(max_num, in_code, is_part_2)
+    count = 0
+    for index, three in threes.items():
+        count += has_fives_in_next_1000(index, *three, fives)
+        if count == 64:
+            return index
+
+
 
 in_code = 'ihaygndm'
 
+# the first argument is the max number of hashes to generate 
+# I just played around with it until I got a solution
+part_1 = find_first_64(16000, in_code)
+part_2 = find_first_64(25000, in_code, True)
 
-#@memoize
-def generate_hash(start_num, condition, limit=None, character=''):
-    number = start_num
-    found = False
-    while not found:
-        number += 1
-        candidate = in_code + str(number)
-        hash_code = hashlib.md5(candidate.encode()).hexdigest()
-        if condition(hash_code, character):
-            return number, hash_code, condition(hash_code, character)
-        if limit and number > limit:
-            return False
-
-
-def find_sixtyfourth_index():
-    number = 0
-    counter = 0
-    while True:
-        number, _, character = generate_hash(number, has_three_consec_characters)
-        if generate_hash(number, has_five_consec_characters, number+1000, character):
-            counter += 1
-            # print(f'{counter}th number is {number}')
-        if counter == 64:
-            return number
-
-
-part_1 = find_sixtyfourth_index()
-print_solutions(part_1)
-# Part 1 solution is: 15035
+print_solutions(part_1, part_2)
+# Part 1 solution is: 15035      ~ 0.4 s
+# Part 2 solution is: 19968      ~ 52 s
