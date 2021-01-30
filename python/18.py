7a4945a2fed5308c464990995d57b82f1f00c38c
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-30 18:40:46 +0100

    [day18] further simplification of safe condition
    no need for a function call, we simply need to check for symmetric
    conditions, ie. that the first character equals the third character.
    The most time is spent on creating a new list with True prepended and
    appended to the original list to take into account the 'implied' spots.
    
    I also switched to lists of booleans, that also reduced the run time a
    bit.

--- python/18.py
+++ python/18.py
@@ -2,30 +2,27 @@
 
 from santas_little_helpers import *
 
-
-def is_safe(left, right):
-    if left == right:
-        return '.'
-    return '^'
-
-
+# the 'is safe' conditions from the puzzle text translate to
+# symmetric configurations of the above row, such as:
+# .^., ^^^, ..., ^.^ - ie, we only need to check if first
+# letter is equal to the last
 def generate_next_row(in_row):
-    above_row = ['.'] + in_row + ['.']
-    return [is_safe(one, three) for one, three in zip(above_row, above_row[2:])]
+    above_row = [True] + in_row + [True]
+    return [one == three for one, three in zip(above_row, above_row[2:])]
 
 
 def count_safe(in_row, total_rows):
     next_row = list(in_row)
-    count = next_row.count('.')
-    for _ in range(total_rows-1):
+    count = 0
+    for _ in range(total_rows):
+        count += sum(next_row)
         next_row = generate_next_row(next_row)
-        count += next_row.count('.')
     return count
 
 
-first_row = [c for c in get_input('inputs/18')[0]]
+first_row = [c=='.' for c in get_input('inputs/18')[0]]
+part_1, part_2 = (count_safe(first_row, rows) for rows in {40, 400000})
 
-part_1, part_2 = (count_safe(generate_map(first_row, rows)) for rows in {40, 400000})
 print_solutions(part_1, part_2)
 # Part 1 solution is: 1989
 # Part 2 solution is: 19999894
