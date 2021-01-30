f08e1660ca571079b446ecee310ddceefc1ec78a
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-30 14:30:10 +0100

    [day18] both parts first go
    now i want to figure out the trick behind it

--- python/18.py
+++ python/18.py
@@ -1,3 +1,40 @@
 # Day 18: Like a Rogue
 
 from santas_little_helpers import *
+
+
+def is_safe(left, center, right):
+    if left == center == '^' and right == '.':
+        return '^'
+    if center == right =='^' and left == '.':
+        return '^'
+    if left =='^' and center == right == '.':
+        return '^'
+    if right =='^' and center == left == '.':
+        return '^'
+    return '.'
+
+
+def generate_next_row(in_row):
+    above_row = ['.'] + in_row + ['.']
+    return [is_safe(one, two, three) for one, two, three in zip(above_row, above_row[1:], above_row[2:])]
+
+
+def generate_map(in_row, total_rows):
+    whole_map = [in_row]
+    for _ in range(total_rows-1):
+        new_row = generate_next_row(whole_map[-1])
+        whole_map.append(new_row)
+    return whole_map
+
+
+def count_safe(in_map):
+    return sum(c == '.' for line in in_map for c in line)
+
+
+first_row = [c for c in get_input('inputs/18')[0]]
+
+part_1, part_2 = (count_safe(generate_map(first_row, rows)) for rows in {40, 400000})
+print_solutions(part_1, part_2)
+# Part 1 solution is: 1989
+# Part 2 solution is: 19999894
