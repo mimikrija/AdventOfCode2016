09ec312100481370d5b65fea532de1ec68d51d99
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-31 10:51:07 +0100

    [day20] simplify search for smallest allowed:
    no need to check the ranges as long as the forbidden ranges are sorted
    by their low value.

--- python/20.py
+++ python/20.py
@@ -4,34 +4,21 @@ from santas_little_helpers import *
 from collections import deque
 
 
+ranges = (tuple(map(int, line.split('-'))) for line in get_input('inputs/20'))
 
-ranges = (list(map(int, line.split('-'))) for line in get_input('inputs/20'))
 
-
-def find_smallest_allowed(in_forbidden):
-    allowed = deque()
-    allowed.append((0, 4294967295))
+def find_lowest_allowed(in_forbidden):
+    lowest_allowed = 0
     forbidden = sorted(in_forbidden, key = lambda x: x[0])
-    for e, f in forbidden:
-        a, b = allowed.pop()
-        if a == e:
-            allowed.append((f+1, b))
-        elif e > b or f < a:
-            allowed.append((a,b))
-        elif a < e < f < b:
-            allowed.append((a, e-1))
-            allowed.append((f+1, b))
-        elif a < e < b < f:
-            allowed.append((a, e-1))
-        elif e < a < f < b:
-            allowed.append((f+1, b))
-        else:
-            continue
-        if len(allowed) == 2:
-            return allowed[0][0]
+    for low, high in forbidden:
+        if low <= lowest_allowed <= high:
+            lowest_allowed = high + 1
+    return lowest_allowed
 
 
 
-part_1 = find_smallest_allowed(ranges)
+part_1 = find_lowest_allowed(ranges)
+
+
 print_solutions(part_1)
 # Part 1 solution is: 31053880
