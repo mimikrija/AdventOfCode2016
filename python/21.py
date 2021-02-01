390f3819050ff84fe46c34bb568092953f262425
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-01 20:31:55 +0100

    [day21] define scrambling functions

--- python/21.py
+++ python/21.py
@@ -6,6 +6,45 @@ from collections import namedtuple
 
 Instruction = namedtuple('Instruction',['command', 'args'])
 
+def swap_position(in_string, pos_1, pos_2):
+    return in_string[:pos_1] + in_string[pos_2] + in_string[pos_1+1:pos_2] + in_string[pos_1] + in_string[pos_2+1:]
+
+def swap_letter(in_string, letter_1, letter_2):
+    return in_string.translate(str.maketrans(letter_1+letter_2, letter_2+letter_1))
+
+def rotate(in_string, rotate_by):
+    return in_string[-rotate_by:] + in_string[:-rotate_by]
+
+def rotate_based(in_string, letter):
+    letter_index = in_string.index(letter)
+    rotate_by = (1 + letter_index + 1*(letter_index >= 4))%len(in_string)
+    return rotate(in_string, rotate_by)
+
+def reverse_positions(in_string, pos_1, pos_2):
+    return in_string[:pos_1] + in_string[pos_1:pos_2+1][::-1] + in_string[pos_2+1:]
+
+def move_position(in_string, pos_1, pos_2):
+    letter = in_string[pos_1]
+    remainder = in_string[:pos_1] + in_string[pos_1+1:]
+    return remainder[:pos_2] + letter + remainder[pos_2:]
+
+def scramble(command, args, in_string):
+    if command == 'swap position':
+        return swap_position(in_string, *sorted(args))
+    if command == 'swap letter':
+        return swap_letter(in_string, *args)
+    if command == 'rotate left':
+        return rotate(in_string, -args[0])
+    if command == 'rotate right':
+        return rotate(in_string, args[0])
+    if command == 'rotate based':
+        return rotate_based(in_string, args[0])
+    if command == 'reverse positions':
+        return reverse_positions(in_string, *sorted(args))
+    if command == 'move position':
+        return move_position(in_string, *args)
+
+
 instructions = []
 for line in get_input('inputs/21'):
     command = ' '.join(word for word in line.split(' ')[:2])
@@ -14,4 +53,3 @@ for line in get_input('inputs/21'):
         instructions.append(Instruction(command, map(int, args)))
     if command in ('swap letter', 'rotate based'):
         instructions.append(Instruction(command, args))
-
