984bde16f1eca05c2e4cee327abd1131a1175297
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-03 20:02:27 +0100

    [day17] define maze grid (4x4)

--- python/17.py
+++ python/17.py
@@ -2,13 +2,19 @@
 
 from santas_little_helpers import *
 from hashlib import md5
+from itertools import product
+
+GRID = {complex(x, y) for x, y in product(range(4), repeat=2)}
+start = 0+0j
+vault = 3_3j
+
+
 
 
 def paths_from_here(passcode, path):
     "returns possible directions based on md5 hash of `passcode`+`path`"
     seed = passcode + ''.join(c for c in path)
     return [direction for code, direction in zip(md5(seed.encode()).hexdigest()[:4], 'UDLR') if int(code, 16) > 10]
-
 
 
 passcode = 'bwnlcvfs'
