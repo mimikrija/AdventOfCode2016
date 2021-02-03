b7c62ff314797d61b02154ca0dae9b53ec7b5ddd
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-03 19:59:25 +0100

    [day17] generate next paths
    this will be used in a bfs algorithm while checking out possible paths.

--- python/17.py
+++ python/17.py
@@ -1,5 +1,14 @@
 # Day 17: Two Steps Forward
 
 from santas_little_helpers import *
+from hashlib import md5
+
+
+def paths_from_here(passcode, path):
+    "returns possible directions based on md5 hash of `passcode`+`path`"
+    seed = passcode + ''.join(c for c in path)
+    return [direction for code, direction in zip(md5(seed.encode()).hexdigest()[:4], 'UDLR') if int(code, 16) > 10]
+
+
 
 passcode = 'bwnlcvfs'
