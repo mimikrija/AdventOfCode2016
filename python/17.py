e249a580fdc39a826ee0a74fa11cec87c2e91eb4
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-03 20:45:57 +0100

    [day17] bfs for part 1

--- python/17.py
+++ python/17.py
@@ -16,10 +16,39 @@ MOVE = {'U':  0-1j,
         }
 
 
-def paths_from_here(passcode, path):
+def paths_from_here(passcode, path_so_far):
     "returns possible directions based on md5 hash of `passcode`+`path`"
-    seed = passcode + ''.join(c for c in path)
+    seed = passcode + ''.join(c for c in path_so_far)
     return [direction for code, direction in zip(md5(seed.encode()).hexdigest()[:4], 'UDLR') if int(code, 16) > 10]
 
 
+def open_neighbors(position, paths_from_coord):
+    return {(position + MOVE[direction], direction) for direction in paths_from_coord if position + MOVE[direction] in GRID}
+
+
+def BFS_shortest_distance(passcode, start, end):
+    "Red blob games BFS with shortest distance from `start` to `end` implementation"
+    frontier = deque()
+    frontier.append((start, []))
+    came_from = dict()
+    came_from[start] = None
+    counter = 0
+    while frontier:
+        counter += 1
+        current_position, path_so_far = frontier.pop()
+        possible_paths = paths_from_here(passcode, path_so_far)
+        if possible_paths:
+            for next_position, direction in open_neighbors(current_position, possible_paths):
+                frontier.appendleft((next_position, path_so_far + [direction]))
+                came_from[next_position] = current_position
+                # early exit if we found the goal (end) point
+                if next_position == end:
+                    return ''.join(c for c in path_so_far + [direction])
+
+
 passcode = 'bwnlcvfs'
+
+
+part_1 = BFS_shortest_distance(passcode, start, vault)
+print_solutions(part_1)
+# Part 1 solution is: DDURRLRRDD
