9b1902ccc5fdc15d7cf8355139a09a02dac37731
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-04 13:53:59 +0100

    [day19] slicing approach is very fast

--- python/19.py
+++ python/19.py
@@ -4,13 +4,15 @@ from collections import deque
 
 total_elves = 3014387
 
-# create a circle
-elves_circle = deque(range(1, total_elves+1))
+
+elves_circle = list(range(1,total_elves+1))
 
 while len(elves_circle) > 1:
-    current_elf = elves_circle.popleft()
-    next_elf = elves_circle.popleft()
-    elves_circle.append(current_elf)
+    if len(elves_circle)%2 == 1:
+        elves_circle = elves_circle[::2][1:]
+    else:
+        elves_circle = elves_circle[::2]
+
 
 part_1 = elves_circle.pop()
 print_solutions(part_1)
