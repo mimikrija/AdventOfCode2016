b29a91ab5cc253d158f0f4b602888b0bbb9ebb69
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-04 12:32:09 +0100

    [day19] deque approach (also slow)

--- python/19.py
+++ python/19.py
@@ -1,20 +1,17 @@
 # Day 19: An Elephant Named Joseph
 from santas_little_helpers import *
+from collections import deque
 
 total_elves = 3014387
 
 # create a circle
-elves_circle = {elf_num: (elf_num * (elf_num < total_elves) + 1, 1) for elf_num in range(1, total_elves+1)}
+elves_circle = deque(range(1, total_elves+1))
 
 while len(elves_circle) > 1:
-    for current_elf in range(1, total_elves+1):
-        if current_elf in elves_circle.keys():
-            points_to, presents = elves_circle[current_elf]
-            points_to_next, presents_to_steal = elves_circle[points_to]
-            if presents_to_steal > 0:
-                elves_circle[current_elf] = (points_to_next, presents + presents_to_steal)
-                del elves_circle[points_to]
+    current_elf = elves_circle.popleft()
+    next_elf = elves_circle.popleft()
+    elves_circle.append(current_elf)
 
-part_1 = list(elves_circle.keys())[0]
+part_1 = elves_circle.pop()
 print_solutions(part_1)
 # Part 1 solution is: 1834471
