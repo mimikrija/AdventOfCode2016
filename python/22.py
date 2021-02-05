90860dd24f4c9190219e4472a11902f362a2932f
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-05 20:46:31 +0100

    [day22] parse input

--- python/22.py
+++ python/22.py
@@ -1,2 +1,10 @@
 # Day 22: Grid Computing
 
+from santas_little_helpers import *
+from re import findall
+from collections import namedtuple
+
+Node = namedtuple('Node', ['x', 'y', 'size', 'used', 'avail', 'use_percent'])
+
+nodes_data = [Node(*list(map(int, findall(r'\d+', line)))) for line in get_input('inputs/22')[2:]]
+
