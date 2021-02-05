c902418bce14672066d8825bd828b13738b95fe7
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-05 11:52:15 +0100

    [day19] replace while loop with a recursive function

--- python/19.py
+++ python/19.py
@@ -1,19 +1,22 @@
 # Day 19: An Elephant Named Joseph
 from santas_little_helpers import *
-from collections import deque
+
 
 total_elves = 3014387
+
+def joseph(in_circle):
+    if len(in_circle) == 1:
+        return in_circle[0]
+    else:
+        if len(in_circle) % 2 == 1:
+            return joseph(in_circle[::2][1:])
+        else:
+            return joseph(in_circle[::2])
 
 
 elves_circle = list(range(1,total_elves+1))
 
-while len(elves_circle) > 1:
-    if len(elves_circle)%2 == 1:
-        elves_circle = elves_circle[::2][1:]
-    else:
-        elves_circle = elves_circle[::2]
 
-
-part_1 = elves_circle.pop()
+part_1 = joseph(elves_circle)
 print_solutions(part_1)
 # Part 1 solution is: 1834471
